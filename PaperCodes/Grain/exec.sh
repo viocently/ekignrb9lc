83885a5ddc362b1ef7bbb76e984e880ef1c3442e
HeJiahui <qq591106627@126.com>
2022-02-18 12:20:50 +0800

    new version

--- PaperCodes/Grain/exec.sh
+++ PaperCodes/Grain/exec.sh
@@ -2,7 +2,3 @@ mkdir LOG
 mkdir STATE
 mkdir TERM
 g++  main.cpp grain.cpp  newgrain.cpp graincallback.cpp graintrack.cpp grainframework.cpp ugrain.cpp -o grain -std=c++11 -O3 -lm -lpthread -I/$GUROBI_HOME/include/ -L/$GUROBI_HOME/lib -lgurobi_c++ -lgurobi91 -lm
-
-
-
-
--- PaperCodes/Kreyvium/exec.sh
+++ PaperCodes/Kreyvium/exec.sh
@@ -2,7 +2,3 @@ mkdir LOG
 mkdir STATE
 mkdir TERM
 g++  main.cpp kreyviumframework.cpp kreyvium.cpp deg.cpp newkreyvium.cpp kreyviumcallback.cpp kreyviumtrack.cpp ukreyvium.cpp  -o kreyvium -std=c++11 -O3 -lm -lpthread -I/$GUROBI_HOME/include/ -L/$GUROBI_HOME/lib -lgurobi_c++ -lgurobi91 -lm
-
-
-
-
